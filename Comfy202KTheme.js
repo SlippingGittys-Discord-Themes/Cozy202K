a21b703a653498f1124b5980cac3ff90c03d2d4c
SlippingGitty <76500838+SlippingGitty@users.noreply.github.com>
2021-01-28 22:14:13 -0600

    Goodmod support

---
+++ Comfy202KTheme.js
@@ -0,0 +1,28 @@
+let version = '2021.01.28.01';
+
+let el;
+
+const css = `@import url("https://slippinggitty.github.io/COMFY202K.css");`;
+
+let obj = {
+    onImport: async function () {
+      el = document.createElement('style');
+  
+      document.head.appendChild(el);
+  
+      el.appendChild(document.createTextNode(css));
+    },
+  
+    remove: async function () {
+      el.remove();
+    },
+  
+    name: 'SlideToUnlock',
+    description: 'A Discord Theme based on LeoRicharte's concept art',
+  
+    author: ['SlippingGitty NYRI4 Tamago-iku'],
+  
+    version
+  };
+  
+  obj
